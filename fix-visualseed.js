64efc834ec5cae6aae1be1bd28900dee9caf7b16
Destiny-AX <3350143673@qq.com>
2026-06-08 23:32:49 +0800

    chore: 删除临时修复脚本

--- fix-visualseed.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = 'd:\\Trae_Solo_Project\\拾寻\\api\\records.js';
-let content = fs.readFileSync(path, 'utf-8');
-
-// 修复所有 visualSeed 行：确保前面有换行和正确缩进
-content = content.replace(/\},\r\n    visualSeed:/g, '\},\n    visualSeed:');
-
-fs.writeFileSync(path, content);
-console.log('Fixed visualSeed newlines');
